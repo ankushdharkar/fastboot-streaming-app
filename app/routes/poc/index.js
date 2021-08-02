65ca82776e2ead29b2c87ac876471907319996c5
ankushdharkar <ankushdharkar@gmail.com>
2021-08-01 17:52:17 -0700

    /poc route to verify the streaming effect of shoebox data

--- app/routes/poc/index.js
+++ app/routes/poc/index.js
@@ -1,13 +1,12 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 
-// Fake api that resolves after 2 seconds.
+// Fake api that resolves after x seconds.
 function getFakeData(id) {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({ id, foo: "Hello Ember!" });
-
-    });
+    }, 5000);
   });
   // return Promise.resolve();
 }
@@ -20,22 +19,52 @@ function wait(ms) {
 
 export default class PocIndexRoute extends Route {
   @service fastboot;
-  id = 123;
-  async model() {
+  id = '123';
+  model() {
     let shoebox = this.fastboot.shoebox;
     let isFastBoot = this.fastboot.isFastBoot;
 
+    console.info('isFastboot', isFastBoot);
+
     if (isFastBoot) {
       return getFakeData(this.id).then(async (data) => {
-        await wait(2000);
+        await wait(1000);
         // Stream the data here
-        shoebox.stream(data.id, JSON.stringify(data));
-        await wait(4000);
-        shoebox.stream('ss', JSON.stringify({ foo: "bar" } ));
+        shoebox.stream('ss', JSON.stringify(data));
+        shoebox.stream('something-else-id', JSON.stringify({id: this.id, foo: "bar" }));
         return data;
       });
     }
 
+    // await wait(100);
+    console.info('SHOEBOX', shoebox);
     return shoebox && shoebox.retrieve(this.id);
   }
 }
+
+
+  // <code style="display: none;" id=shoebox-123> {"id":123,"foo":"Hello Ember!"}</code>
+  // <code style="display: none;" id=shoebox-123> {"id":123,"foo":"bar"} </code>
+  // <script type="x/boundary" id="fastboot-body-start"></script>
+/*  Usual way:
+    const shoebox = this.fastboot.shoebox;
+    let shoeboxStore = shoebox.retrieve('my-store');
+    const isFastBoot = this.fastboot.isFastBoot;
+
+    const PERSON_ID = 2;
+
+    if (isFastBoot) {
+      return this.store.find('person', PERSON_ID).then((data) => {
+        if (!shoeboxStore) {
+          shoeboxStore = {};
+          shoebox.put('my-store', shoeboxStore);
+        }
+
+        shoeboxStore[PERSON_ID] = JSON.stringify(data);
+      });
+    }
+
+    console.info('SHOEBOX', shoeboxStore);
+
+    return shoeboxStore && shoeboxStore[PERSON_ID];
+*/
